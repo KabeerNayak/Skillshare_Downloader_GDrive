ba4ca2496b3d3561fa239de40c5b472ffecdfbca
KabeerNayak <81094260+KabeerNayak@users.noreply.github.com>
2021-03-27 22:54:45 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# SkillshareDownloader
+
+|Help|
+
+1.Log-in to skillshare in your browser and open up the developer console. (cmd-shift-c for chrome on mac)
+
+2.Use it to grab your cookie by typing: document.cookie
+
+3.Copy-paste cookie from developer console (without " if present) into example script.
+
+
+<h4>📝 Note: Downloader core is not written by me. i found this python downloader from a  forum. i did few modification to code then implemented it into colab. all credit goes to original python script dev.</h4>
+
+#
+## Disclaimer
+
+I will not be responsible for how you use SkillshareDownloader
+Although the downloaded files are not the contents themselves, accessing or storing these files might still be illegal in some parts of the world. So, take great care when using this tool and make sure that it is legal.
---
+++ k_downloader.py
@@ -0,0 +1,45 @@
+import sys, os
+from skillshare import Skillshare
+from stm import cookie
+
+# or by class ID:
+# dl.download_course_by_class_id(189505397)
+
+def main(cookie):
+    temp = sys.argv[2]
+    if len(temp) != 0:
+        cookie = temp
+    #trial
+    ###
+    dl = Skillshare(cookie)
+    course_url = sys.argv[1]
+    dl.download_course_by_url(course_url)
+
+
+def info():
+    print(r"""	 
+                                                        
+ #####  ###### #    # #       ####  ##### #    #  ####  
+ #    # #      ##   # #      #    #   #   #    # #      
+ #####  #####  # #  # #      #    #   #   #    #  ####  
+ #    # #      #  # # #      #    #   #   #    #      # 
+ #    # #      #   ## #      #    #   #   #    # #    # 
+ #####  ###### #    # ######  ####    #    ####   ####  
+
+   #####         #                                                        
+ #     # #    # #    #    ##   #    # #    # #####  #    #   ##   #    # 
+ # ### # #   #  #    #   #  #  ##   # #    # #    # #    #  #  #  #    # 
+ # ### # ####   #    #  #    # # #  # #    # #####  ###### #    # #    # 
+ # ####  #  #   ####### ###### #  # # #    # #    # #    # ###### #    # 
+ #       #   #       #  #    # #   ## #    # #    # #    # #    #  #  #  
+  #####  #    #      #  #    # #    #  ####  #####  #    # #    #   ##   
+                                                                         
+                                                        
+				Visit Us for more Cool Stuff: https://forums.benlotus.com/
+
+                """)
+
+
+if __name__ == "__main__":
+    info()
+    main(cookie)
